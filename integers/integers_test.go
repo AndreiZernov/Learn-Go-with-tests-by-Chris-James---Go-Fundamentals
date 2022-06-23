f065766a08ce3bc5234e39eb7a33b33bac1125a1
Andrei Zernov <andrei.zernov@outlook.com>
2022-06-23 20:58:54 +0100

    feat: array and slices - sumAllTails

--- integers/integers_test.go
+++ integers/integers_test.go
@@ -3,7 +3,7 @@ package integers
 import "testing"
 
 func TestAdder(t *testing.T) {
-	assertCorrectMessage := func(t testing.TB, expected, sum int) {
+	checkAdder := func(t testing.TB, expected, sum int) {
 		t.Helper()
 		if expected != sum {
 			t.Errorf("expected %d sum %d", expected, sum)
@@ -13,11 +13,11 @@ func TestAdder(t *testing.T) {
 	t.Run("expected 2 + 2 equal 4", func(t *testing.T) {
 		sum := Add(2, 2)
 		expected := 4
-		assertCorrectMessage(t, expected, sum)
+		checkAdder(t, expected, sum)
 	})
 	t.Run("expected 2 + 6 equal 8", func(t *testing.T) {
 		sum := Add(6, 2)
 		expected := 8
-		assertCorrectMessage(t, expected, sum)
+		checkAdder(t, expected, sum)
 	})
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -9,7 +9,7 @@ func BenchmarkRepeat(b *testing.B) {
 }
 
 func TestRepeat(t *testing.T) {
-	assertCorrectMessage := func(t testing.TB, repeated, expected string) {
+	checkRepeter := func(t testing.TB, repeated, expected string) {
 		t.Helper()
 		if expected != repeated {
 			t.Errorf("expected %q but got %q", expected, repeated)
@@ -19,12 +19,12 @@ func TestRepeat(t *testing.T) {
 	t.Run("expected a runs 5 times and equal aaaaa", func(t *testing.T) {
 		repeated := Repeat("a", 5)
 		expected := "aaaaa"
-		assertCorrectMessage(t, repeated, expected)
+		checkRepeter(t, repeated, expected)
 	})
 
 	t.Run("expected a runs 3 times and equal aaa", func(t *testing.T) {
 		repeated := Repeat("a", 3)
 		expected := "aaa"
-		assertCorrectMessage(t, repeated, expected)
+		checkRepeter(t, repeated, expected)
 	})
 }
