943577a3bde6ae8380c40ac6409937f9532e6543
Andrei Zernov <andrei.zernov@outlook.com>
2022-07-16 11:12:12 +0100

    test: add more tests to array and slices exercise

--- array_and_slices/sum_test.go
+++ array_and_slices/sum_test.go
@@ -21,6 +21,14 @@ func TestSum(t *testing.T) {
 		want := 15
 		checkSum(t, got, want, numbers)
 	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+		checkSum(t, got, want, numbers)
+	})
 }
 
 func TestSumAll(t *testing.T) {
@@ -34,6 +42,12 @@ func TestSumAll(t *testing.T) {
 	t.Run("sum of two slices", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
+		checkSumAll(t, got, want)
+	})
+
+	t.Run("sum of two slices with different capacity at each slice", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3, 4, 5}, []int{6, 7, 8, 9})
+		want := []int{15, 30}
 		checkSumAll(t, got, want)
 	})
 }
@@ -56,6 +70,12 @@ func TestSumAllTails(t *testing.T) {
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum of tails of two slices with different capacity at each slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3, 4, 5}, []int{6, 7, 8, 9})
+		want := []int{14, 24}
 		checkSums(t, got, want)
 	})
 }
