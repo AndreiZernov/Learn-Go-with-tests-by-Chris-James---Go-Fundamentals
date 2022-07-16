a65fcc4c39b6c9bb0eba85132b2ad047d37378fc
Andrei Zernov <andrei.zernov@outlook.com>
2022-07-16 14:29:19 +0100

    refactor: add assertError for error handling

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -5,13 +5,29 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
 
-	assertString(t, got, want)
+		assertStrings(t, got, "this is just a test")
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+
 }
 
-func assertString(t *testing.T, got, want string) {
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
