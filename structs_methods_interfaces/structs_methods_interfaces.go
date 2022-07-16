0f12c34172284f05b2f52ae327422d2c3bc9478d
Andrei Zernov <andrei.zernov@outlook.com>
2022-07-16 12:18:26 +0100

    feat: add the triangle for area and parameters

--- structs_methods_interfaces/structs_methods_interfaces.go
+++ structs_methods_interfaces/structs_methods_interfaces.go
@@ -31,3 +31,16 @@ func (c Circle) Perimeter() float64 {
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Perimeter() float64 {
+	return t.Base + t.Height + math.Sqrt(math.Pow(t.Base, 2)+math.Pow(t.Height, 2))
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
--- structs_methods_interfaces/structs_methods_interfaces_test.go
+++ structs_methods_interfaces/structs_methods_interfaces_test.go
@@ -11,6 +11,7 @@ func TestPerimeter(t *testing.T) {
 	}{
 		{Rectangle{10, 10}, 40.0},
 		{Circle{10}, 62.83185307179586},
+		{Triangle{8, 4}, 20.94427190999916},
 	}
 
 	for _, tt := range perimeterTests {
@@ -28,6 +29,7 @@ func TestArea(t *testing.T) {
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
