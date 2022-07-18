21c2b774f0713744fef2f581d946b435c1790927
Andrei Zernov <andrei.zernov@outlook.com>
2022-07-18 13:12:40 +0100

    refactor: mocking

--- mocking/main.go
+++ mocking/main.go
@@ -8,30 +8,25 @@ import (
 )
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
-
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
-const finalWord = "Go!"
-const countdownStart = 3
 
 type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
+
+const finalWord = "Go!"
+const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
